6b2c527448873234be22e163e6a7ac235b56510d
David-Portugal-Ferreira <davidportugal44@gmail.com>
2025-02-14 16:32:42 +0000

    Add background images to homepage cards.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,10 +2,21 @@ import { Link } from "react-router";
 import useGetData from "../../hooks/getData";
 import HomePageCards from "../HomePageCards/HomePageCards";
 import Loading from "../Loading";
-import styles from "./HomePage.module.css"
+import styles from "./HomePage.module.css";
+import electronicsImg from "../../assets/electronics.jpg";
+import jewelryImg from "../../assets/jewelry.jpg";
+import menClothingImg from "../../assets/menclothing.jpg";
+import womenClothingImg from "../../assets/womenclothing.jpg";
 
 function HomePage() {
   const { data, loading, error } = useGetData("products/categories");
+
+  const categoryImages = {
+    "electronics": electronicsImg,
+    "jewelery": jewelryImg,
+    "men's clothing": menClothingImg,
+    "women's clothing": womenClothingImg,
+  };
 
   if (loading) return <Loading />;
   if (error) return <Error />;
@@ -13,8 +24,12 @@ function HomePage() {
   return (
     <div className={styles["homepage-wrapper"]}>
       <div className={styles["homepage-cards"]}>
-        {data.map((categorie, index) => (
-          <HomePageCards key={index} name={categorie} />
+        {data.map((category, index) => (
+          <HomePageCards
+            key={index}
+            name={category}
+            image={categoryImages[category]}
+          />
         ))}
       </div>
       <div className={styles.shopButton}>
--- src/components/HomePageCards/HomePageCards.jsx
+++ src/components/HomePageCards/HomePageCards.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router";
 import styles from "./HomePageCards.module.css"
 
-function HomePageCards({ name }) {
+function HomePageCards({ name, image }) {
   return (
     <Link to={`shop/:${name}`}>
-      <div className={styles["homepage-individual-card"]}>
+      <div className={styles["homepage-individual-card"]} style={{backgroundImage: `url(${image})`}}>
         <h1>{name}</h1>
       </div>
     </Link>
